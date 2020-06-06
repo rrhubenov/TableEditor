5891e892835dbfb77f3ad6efcca34a3d91d598b2
Radoslav Hubenov <rrhubenov@gmail.com>
2020-06-06 19:19:36 +0300

    Scaffold for the Cell system

--- include/table/cell.hh
+++ include/table/cell.hh
@@ -3,12 +3,43 @@
 #include <string>
 
 class Cell {
-    private:
+    protected:
+        int row;
+        int column;
         std::string value;
     public:
-        Cell(std::string value);
+        Cell(int row, int column, std::string value);
 
-        std::string getValue();
-        void setValue(std::string value);
+        virtual double parse() = 0;
+        virtual void print() = 0;
+
+        int getRow() const;
+        int getColumn() const;
+        std::string getRawValue() const;
+
+        virtual double operator+(Cell*) = 0; 
+        virtual double operator*(Cell*) = 0;
+        virtual double operator/(Cell*) = 0;
 };
+
+
+class FormulaCell: public Cell {
+    private:
+        Cell* lhsCell;
+        Cell* rhsCell;
+};
+
+class StringCell: public Cell {
+    public:
+        StringCell(int row, int column, std::string value);
+
+        double parse();
+        void print();
+
+        double operator+(Cell*);
+        double operator*(Cell*);
+        double operator/(Cell*);
+};
+
+class DoubleCell: public Cell {};
 
--- src/table/cell.cpp
+++ src/table/cell.cpp
@@ -1,14 +1,39 @@
 #include "../../include/table/cell.hh"
+#include <iostream>
 #include <string>
 
-Cell::Cell(std::string value) {
-    this->value = value;
-}
-
-std::string Cell::getValue() {
+std::string Cell::getRawValue() const {
     return this->value;
 }
 
-void Cell::setValue(std::string value) {
-    this->value = value; 
+int Cell::getRow() const {
+    return this->row;
+}
+
+int Cell::getColumn() const {
+    return this->column;
+}
+
+Cell::Cell(int row, int column, std::string value): row(row), column(column), value(value){}
+
+StringCell::StringCell(int row, int column, std::string value): Cell(row, column, value){}
+
+double StringCell::parse() {
+    return 1;
+}
+
+void StringCell::print() {
+    std::cout << this->value;
+}
+
+double StringCell::operator+(Cell* other) {
+    return this->parse()+other->parse();
+}
+
+double StringCell::operator*(Cell* other) {
+    return this->parse()*other->parse();
+}
+
+double StringCell::operator/(Cell* other) {
+    return this->parse()/other->parse();
 }
