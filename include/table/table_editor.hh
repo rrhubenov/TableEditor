2d99c95421c8446c5f43701ae853a17fb47ca270
Radoslav Hubenov <rrhubenov@gmail.com>
2020-06-07 23:00:38 +0300

    Methods for handling saving changes

--- include/table/table_editor.hh
+++ include/table/table_editor.hh
@@ -12,11 +12,22 @@ class TableEditor: public IObserver {
     private:
         FileManager* fileManager;
         std::vector<Row*> rows;
+        
+        std::string toRaw();
 
     public:
         TableEditor(FileManager* fileManager);
 
+        void save();
+        void saveAs(std::string file_path);
+
+        void addCell(unsigned row, unsigned column, std::string value);
+
         void update(std::vector<std::string> lines);
+
+        void print();
+
+        void destroyRows();
 
         ~TableEditor();
 };
