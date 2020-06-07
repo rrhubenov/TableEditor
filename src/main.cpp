fc56d087cad16b9a9bd131e13d153487e0003250
Radoslav Hubenov <rrhubenov@gmail.com>
2020-06-07 22:57:51 +0300

    register all the commands

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "../include/file/file_manager.hh"
 #include "../include/table/table_editor.hh"
 #include "../include/cli/cli_controller.hh"
+#include "../include/cli/commands.hh"
 
 #include <iostream>
 #include <string>
@@ -12,6 +13,20 @@ int main(int argc, char **argv) {
     file_manager.attach(&table_editor);
 
     CLIController cli_controller;
+
+    Open* openCommand = new Open(&file_manager);
+    Print* printCommand = new Print(&table_editor);
+    SaveAs* saveAsCommand = new SaveAs(&table_editor);
+    Edit* editCommand = new Edit(&table_editor);
+    Save* saveCommand = new Save(&table_editor);
+    Exit* exitCommand = new Exit();
+
+    cli_controller.register_command(openCommand);
+    cli_controller.register_command(printCommand);
+    cli_controller.register_command(saveAsCommand);
+    cli_controller.register_command(editCommand);
+    cli_controller.register_command(saveCommand);
+    cli_controller.register_command(exitCommand);
 
     cli_controller.begin_reading();
 
